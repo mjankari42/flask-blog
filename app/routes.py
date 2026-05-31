8774d3387be410132c1290f8754c0308743aeeba
Mahdi Jankari <mjankari42@gmail.com>
2026-06-01 01:24:39 +0300

    user jinja html template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
+
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world!"
+    user = {"username": "Mahdi"}
+    posts = [
+        {
+            "author": {"username": "Bob"},
+            "body": "What does six seven even mean?"
+        },
+        {
+            "author": {"username": "Kid"},
+            "body": "six seven, six seven, six seven, six seven, ..."
+        }
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
