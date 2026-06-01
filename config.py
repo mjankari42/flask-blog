4a5f5a2d7798fc4e46e0fabdcb40583f837c412e
Mahdi Jankari <mjankari42@gmail.com>
2026-06-01 16:33:04 +0300

    configure flask sqlalchemy

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+from pathlib import Path
+
+base_directory = Path(__file__).parent
+default_db = f"sqlite:///{Path(base_directory) / 'app.db'}"
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-password-safe"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_db
