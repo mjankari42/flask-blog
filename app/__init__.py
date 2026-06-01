44b1ed772b83ac02f62dbe2c0062ed4480ffced3
Mahdi Jankari <mjankari42@gmail.com>
2026-06-01 19:20:08 +0300

    setup flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,5 +9,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
-from app import routes, models
+from app import models, routes  # noqa: E402, F401
