cc13d9692229b0615fb8083a7ea3ef4e63240205
Mahdi Jankari <mjankari42@gmail.com>
2026-06-02 14:37:14 +0300

    add user profile page

--- app/routes.py
+++ app/routes.py
@@ -72,3 +72,14 @@ def register():
         return redirect(url_for("login"))
 
     return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = db.first_or_404(select(User).where(User.username == username))
+    posts = [
+        {"author": user, "body": "test post 1"},
+        {"author": user, "body": "test post 2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
