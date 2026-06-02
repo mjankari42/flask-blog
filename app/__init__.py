a6db07f42ba076fb6d4708aae45622de016becfa
Mahdi Jankari <mjankari42@gmail.com>
2026-06-02 17:52:33 +0300

    add a way to log info and warnings

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import pathlib
+import os
 
 from flask import Flask
 from flask_login import LoginManager
@@ -15,23 +17,39 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"  # type: ignore[assignment]
 
-if not app.debug and app.config["MAIL_SERVER"]:
-    auth = None
-    if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-        auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-    secure = None
-    if app.config["MAIL_USE_TLS"]:
-        secure = ()
-    mail_handler = SMTPHandler(
-        mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
-        fromaddr="no-reply@" + app.config["MAIL_SERVER"],
-        toaddrs=app.config["ADMINS"],
-        subject="Blog Failure",
-        credentials=auth,
-        secure=secure,
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"],
+            subject="Blog Failure",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not pathlib.Path("logs").exists():
+        os.mkdir("logs")
+
+    file_handler = RotatingFileHandler("logs/blog.log", maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+        )
     )
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info("Blog startup")
 
 
 from app import errors, models, routes  # noqa: E402, F401
