ad19cfb56d8e6e1794af2cf9bd1ef3db1e6c5bfe
Mahdi Jankari <mjankari42@gmail.com>
2026-06-02 17:44:00 +0300

    add email report in case of error

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -12,5 +15,23 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = "login"  # type: ignore[assignment]
 
+if not app.debug and app.config["MAIL_SERVER"]:
+    auth = None
+    if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+        auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+    secure = None
+    if app.config["MAIL_USE_TLS"]:
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+        fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+        toaddrs=app.config["ADMINS"],
+        subject="Blog Failure",
+        credentials=auth,
+        secure=secure,
+    )
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
-from app import models, routes, errors  # noqa: E402, F401
+
+from app import errors, models, routes  # noqa: E402, F401
--- config.py
+++ config.py
@@ -4,6 +4,14 @@ from pathlib import Path
 base_directory = Path(__file__).parent
 default_db = f"sqlite:///{Path(base_directory) / 'app.db'}"
 
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-password-safe"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or default_db
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or default_db
+
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = ["your-email@example.com"]
